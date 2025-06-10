dc77dd56c8267b8d67fe3ae0a9cf8485e7370872
Diego Armando <fordiego44@gmail.com>
2025-06-10 06:12:44 -0500

    otro cambio

---
+++ msvc-cursos/src/main/java/org/condori/springcloud/msvc/cursos/repositories/CursoRepository.java
@@ -0,0 +1,7 @@
+package org.condori.springcloud.msvc.cursos.repositories;
+
+import org.condori.springcloud.msvc.cursos.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+}
